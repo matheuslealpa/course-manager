72b4b3cf256f4f5b9664b388d499461f293bbf33
Matheus Leal Gomes da Silva <matheusleal@tre-pa.jus.br>
2022-07-14 10:20:11 -0300

    Protegendo Rotas com Guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import {CourseListComponent} from "./courses/course-list.component";
 import {StarComponent} from "./star/star.component";
 import {ReplacePipe} from "./pipe/replace.pipe";
+import {NavBarComponents} from "./nav-bar/nav-bar.components";
+import {RouterModule} from "@angular/router";
+import {Erro404Component} from "./erro-404/erro-404.component";
+
 
 @NgModule({
   declarations: [
@@ -13,10 +16,27 @@ import {ReplacePipe} from "./pipe/replace.pipe";
     CourseListComponent,
     StarComponent,
     ReplacePipe,
+    NavBarComponents,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'courses',
+        pathMatch: 'full'
+      },
+      {
+        path: 'courses',
+        component: CourseListComponent,
+      },
+      {
+        path: '**',
+        component: Erro404Component
+
+      }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/nav-bar/nav-bar.components.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.components.html'
+})
+export class NavBarComponents{
+
+}
